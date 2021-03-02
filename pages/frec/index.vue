e7aae86a82d12cc8487bc320536c922860759e1e
George Deeks <george@neontribe.co.uk>
2021-03-02 23:02:06 +0000

    fix: search ec

--- pages/frec/index.vue
+++ pages/frec/index.vue
@@ -9,8 +9,13 @@
         </a>
       </div>
       <div class="control is-expanded has-icons-right">
-        <input class="input is-rounded  is-medium" type="text" placeholder="" v-model="searchInput"
-               @input="search(searchInput)">
+        <input 
+          class="input is-rounded is-medium" 
+          type="text" 
+          placeholder="" 
+          v-model="searchInput"
+          @input="search(searchInput)"
+        >
         <span class="icon is-small is-right">
           <font-awesome-icon :icon="['fas', 'spinner']" v-if="isSearching" class="fa-spin"/>
           <font-awesome-icon :icon="['fas', 'search']" v-if="!isSearching"/>
@@ -19,43 +24,18 @@
 
     </div>
 
-
     <div class="dropdown-menu is-center-custom" v-bind:class="{'is-block':results.length}" style="margin-top:-10px;">
       <div class="dropdown-content">
         <div class="make-scrollable">
 
-          <a v-for="result in results" class="dropdown-item">
-            <span>{{result.ec}} / {{result.fr}} - {{result.volume}}</span>
+          <a :key="result" v-for="result in results" class="dropdown-item">
+            <span>{{result.ec}} / {{result.fr}} - {{result.volume}}µl</span>
           </a>
 
         </div>
       </div>
     </div>
     </div>
-
-    <div class="columns" v-for="i in Math.ceil(results.length / 6)">
-      <div class="column is-2" v-for="result in results.slice((i - 1) * 6, i * 6)">
-        <div class="card">
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="title is-4">John Smith</p>
-                <p class="subtitle is-6">@johnsmith</p>
-              </div>
-            </div>
-
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-              <a href="#">#css</a> <a href="#">#responsive</a>
-              <br>
-              <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
 
   </div>
 </template>
